990389e0d59ca355ffa3568d21f10b9fe98692a8
Rohan Kumar <rkrohanrk065@gmail.com>
2020-02-28 16:39:19 +0530

    Updates

---
+++ precache-manifest.973134c51b71be7b1d43def22c5e6bbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dae21530b9d0e3042bd60353661f57a8",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "43177f3504b7e1047caa",
+    "url": "/deploy-react/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1e3dfef9266d1f5382e5",
+    "url": "/deploy-react/static/js/2.a9a89eb1.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/deploy-react/static/js/2.a9a89eb1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "43177f3504b7e1047caa",
+    "url": "/deploy-react/static/js/main.3626923d.chunk.js"
+  },
+  {
+    "revision": "0b9fda09de6e0e53bbe4",
+    "url": "/deploy-react/static/js/runtime-main.0054754c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
